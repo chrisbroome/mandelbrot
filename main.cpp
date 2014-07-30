c2ef2b0079109cf90bfd80f1e37e5292afe438ed
Chris Broome <chrisbroome7@gmail.com>
2014-07-29 23:55:00 -0400

    Move rendering out to a separate method

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 
 unsigned int mandelbrot(const sf::Vector2<double> c, const unsigned int numIterations);
+void updateView(sf::Uint8* pixels, const sf::Rect<double> view, const sf::Vector2u textureSize);
 
 int main() {
 
@@ -14,27 +15,11 @@ int main() {
   const sf::Vector2u textureSize = texture.getSize();
   sf::Uint8 pixels[textureSize.x * textureSize.y * 4];
 
-  const sf::Vector2<double> viewPosition(-2, -2);
-  const sf::Vector2<double> viewSize(4, 4);
-  const sf::Rect<double> view(viewPosition, viewSize);
+  sf::Vector2<double> viewPosition(-2, -2);
+  sf::Vector2<double> viewSize(4, 4);
+  sf::Rect<double> view(viewPosition, viewSize);
 
-  sf::Vector2<double> c(viewPosition);
-  sf::Vector2<double> cInc(viewSize.x / textureSize.x, viewSize.y / textureSize.y);
-
-  // double aInc = view.width / textureSize.x;
-  // double bInc = view.height / textureSize.y;
-
-  unsigned int i = 0;
-  for(pi.y = 0, c.y = viewPosition.y; pi.y < textureSize.y; ++pi.y, c.y += cInc.y) {
-    for(pi.x = 0, c.x = viewPosition.x; pi.x < textureSize.x; ++pi.x, c.x += cInc.x, i += 4) {
-      auto count = mandelbrot(c, 255);
-      sf::Color color(count, count, count);
-      pixels[i] = color.r;
-      pixels[i+1] = color.g;
-      pixels[i+2] = color.b;
-      pixels[i+3] = color.a;
-    }
-  }
+  updateView(pixels, view, textureSize);
 
   // while window is open
   while(window.isOpen()) {
@@ -56,11 +41,27 @@ int main() {
   return 0;
 }
 
+void updateView(sf::Uint8* pixels, const sf::Rect<double> view, const sf::Vector2u textureSize) {
+  sf::Vector2u pi;
+  sf::Vector2<double> c(view.left, view.top);
+  const sf::Vector2<double> cInc(view.width / textureSize.x, view.height / textureSize.y);
+  unsigned int i = 0;
+  for(pi.y = 0, c.y = view.top; pi.y < textureSize.y; ++pi.y, c.y += cInc.y) {
+    for(pi.x = 0, c.x = view.left; pi.x < textureSize.x; ++pi.x, c.x += cInc.x, i += 4) {
+      auto count = mandelbrot(c, 255);
+      sf::Color color(count*8, count*8, count*8);
+      pixels[i] = color.r;
+      pixels[i+1] = color.g;
+      pixels[i+2] = color.b;
+      pixels[i+3] = color.a;
+    }
+  }
+}
+
 unsigned int mandelbrot(const sf::Vector2<double> c, const unsigned int numIterations)
 {
   unsigned int count = 0;
   sf::Vector2<double> z(0, 0);
-//  sf::Vector2<double> t;
 
   if (c.x < -2 || c.x > 2 || c.y < -2 || c.y > 2) return 0;
   for (; count < numIterations; ++count)
@@ -68,8 +69,6 @@ unsigned int mandelbrot(const sf::Vector2<double> c, const unsigned int numItera
     if (z.x*z.x + z.y*z.y > 4) return count;
 
     z = sf::Vector2<double>(z.x*z.x - z.y*z.y + c.x, 2 * z.x*z.y + c.y);
-//    z.x = t.x * t.x - t.y * t.y + c.x; // x^2 - y^2 + a
-//    z.y = 2 * t.x * t.y + c.y;         // 2xy + b
   }
   return z.x*z.x + z.y*z.y < 4 ? 0 : count;
 }
